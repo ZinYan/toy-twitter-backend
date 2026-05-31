c45a1f1292510ba79355872f560147d3d6a92256
Zin Wai Yan <zinwaiyan2226@gmail.com>
2026-05-31 16:41:02 +0900

    [fix] Add tweet user join column

--- src/main/java/me/zinwaiyan/twitter/tweet/domain/Tweet.java
+++ src/main/java/me/zinwaiyan/twitter/tweet/domain/Tweet.java
@@ -20,6 +20,7 @@ public class Tweet extends BaseEntity {
     private Long tweetId;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Column(nullable = false, length = 280)
